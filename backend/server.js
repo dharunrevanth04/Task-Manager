9dc3729c778b4cfa5aa8a3a0ece7d7f7c96b6fa0
dharunrevanth04 <dharunrevanth04@gmail.com>
2026-04-23 20:42:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,13 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
+    
+app.listen(process.env.PORT || 3000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.log(err));
+
+
+
+
